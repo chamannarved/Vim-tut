9ef4cb7a0c7fe363e8c4bf4f1097c02635199758
chamannarved <chamannarved@gmail.com>
2020-01-28 19:09:16 +0530

    Create index.php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<DOCTYPE="html">
+<html>
+	<head>
+		<title>Vim.tut</title>
+	</head>
+	<body>
+		<h1>Vim Tutorial website</h1>
+		<p>
+			This is website for Vim text editor and its use. This whole website code is written in Vim text editor (Programmers text editor).
+		</p>
+	</body>
+</html>
