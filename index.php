8956d48982d2613467b1a35facc6a01766cd98f8
chamannarved <chamannarved@gmail.com>
2020-01-28 20:01:32 +0530

    Update index.php file

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
     <body>
         <h1>Vim Tutorial website</h1>
         <p>
-            This is website for Vim text editor and its use. This whole website code is written in Vim text editor (Programmers text editor).
+	    This is website for Vim text editor 
+	    and its use. This whole website code 
+	    is written in Vim text editor 
+            (Programmers text editor).
 	</p>
     </body>
 </html>
